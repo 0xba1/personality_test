382622daebda065e481feba51c095ca4ac09f93e
0xba1 <dev.boluwatife@gmail.com>
2022-04-13 11:43:31 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Personality Test',
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       home: const Home(
